488b7bef26b8dbff0f6e4235efd55adedc342bc1
SNGCHN LEE <sngchn0312@naver.com>
2024-05-23 23:32:56 +0900

    라우터, 상세 페이지 생성

---
+++ src/pages/Detail.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import { useParams, useHistory } from "react-router-dom";
+
+const detailContainer = styled.div``;
+
+const Detail = () => {
+  return (
+    <>
+      <detailContainer></detailContainer>
+    </>
+  );
+};
+
+export default Detail;
---
+++ src/routes/router.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import HomePage from "../components/HomePage";
+import Detail from "../pages/Detail";
+
+const router = () => {
+  return <div></div>;
+};
+
+export default router;
